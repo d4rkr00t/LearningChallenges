93668a925c74990a0c33f3043af6c80427da2774
Stanislav Sysoev <d4rkr00t@gmail.com>
2017-03-07 21:57:57 +1100

    day(8): Go – Project MVP

--- programming-languages-30-days-challenge/7-9_go/listening-comprehension-calculator/lcc.go
+++ programming-languages-30-days-challenge/7-9_go/listening-comprehension-calculator/lcc.go
@@ -5,22 +5,63 @@ import (
 	"io/ioutil"
 	"path"
 	"regexp"
+	"sort"
 	"strings"
+
+	"math"
+
+	"github.com/fatih/color"
 )
 
 // FileInfo – Information about file
 type FileInfo struct {
 	name  string
 	data  string
-	score int
+	score float32
 }
 
 var wrongGuesses = regexp.MustCompile(`\|(.*?)\|`)
 
-func calcFileScore(data string) int {
+func calcFileWrongGuesses(wrong []string) int {
+	result := 0
+
+	for _, w := range wrong {
+		result += len(strings.Split(w, " "))
+	}
+
+	return result
+}
+
+func calcFileScore(data string) float32 {
 	totalNumOfWords := len(strings.Split(data, " "))
-	fmt.Println(wrongGuesses.FindAllString(data, -1))
-	return totalNumOfWords
+	totalWrongGuesses := calcFileWrongGuesses(wrongGuesses.FindAllString(data, -1))
+	return 100.0 - (float32(totalWrongGuesses) / float32(totalNumOfWords) * 100.0)
+}
+
+func avgScore(files []FileInfo) float32 {
+	var result float32
+
+	for _, file := range files {
+		result += file.score
+	}
+
+	return result / float32(len(files))
+}
+
+type ByScore []FileInfo
+
+func (a ByScore) Len() int           { return len(a) }
+func (a ByScore) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByScore) Less(i, j int) bool { return a[i].score < a[j].score }
+
+func buildMetrics(files []FileInfo, percentile float64) (float32, float32, float32, float32, string, string) {
+	sort.Sort(ByScore(files))
+	avg := avgScore(files)
+	min := files[0].score
+	max := files[len(files)-1].score
+	percentilePos := int(math.Floor(float64(len(files))/100.0*percentile)) - 1
+
+	return avg, min, max, files[percentilePos].score, files[0].name, files[len(files)-1].name
 }
 
 func getFiles(dir string) []string {
@@ -47,5 +88,22 @@ func getFilesInfo(dir string, files []string) []FileInfo {
 func main() {
 	dir := "./data"
 	files := getFilesInfo(dir, getFiles(dir))
-	fmt.Println(files)
+	avg, min, max, percentile, worstFile, bestFile := buildMetrics(files, 80.0)
+
+	red := color.New(color.FgRed).SprintFunc()
+	redBg := color.New(color.FgBlack).Add(color.BgRed).SprintFunc()
+	yellow := color.New(color.FgYellow).SprintFunc()
+	yellowBg := color.New(color.FgBlack).Add(color.BgYellow).SprintFunc()
+	green := color.New(color.FgGreen).SprintFunc()
+	greenBg := color.New(color.FgBlack).Add(color.BgGreen).SprintFunc()
+
+	fmt.Println()
+	fmt.Printf("Worst:          %s\n", red(worstFile))
+	fmt.Printf("Min score:      %s\n", redBg(min))
+	fmt.Println()
+	fmt.Printf("Best:           %s\n", green(bestFile))
+	fmt.Printf("Max score:      %s\n", greenBg(max))
+	fmt.Println()
+	fmt.Printf("Avg score:      %s\n", yellow(avg))
+	fmt.Printf("%s percentile: %s\n", "80%", yellowBg(percentile))
 }

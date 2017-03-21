0291e8b86c9a6ab9816e859c5176284cb388b3b8
Stanislav Sysoev <d4rkr00t@gmail.com>
2017-03-21 20:30:30 +1100

    day(13): PureScript – Hello World 3

---
+++ programming-languages-30-days-challenge/13-15_purescript/address-book/src/Data/AddressBook.purs
@@ -0,0 +1,47 @@
+module Data.AddressBook where
+
+import Prelude
+import Control.Bind ((=<<))
+import Control.Plus (empty)
+import Data.List (List(..), filter, head)
+import Data.Maybe (Maybe)
+
+type Entry =
+    { firstName :: String
+    , lastName  :: String
+    , address   :: Address
+    }
+
+type Address =
+    { street :: String
+    , city   :: String
+    , state  :: String
+    }
+
+type AddressBook = List Entry
+
+showEntry :: Entry -> String
+showEntry entry =
+    entry.lastName <> ", " <>
+    entry.firstName <> ": " <>
+    showAddress entry.address
+
+showAddress :: Address -> String
+showAddress addr =
+    addr.street <> ", " <>
+    addr.city <> ", " <>
+    addr.state
+
+emptyBook :: AddressBook
+emptyBook = empty
+
+insertEntry :: Entry -> AddressBook -> AddressBook
+insertEntry = Cons
+
+findEntry :: String -> String -> AddressBook -> Maybe Entry
+findEntry firstName lastName book =
+    head $ filter filterEntry book
+        where
+            filterEntry :: Entry -> Boolean
+            filterEntry entry = entry.firstName == firstName && entry.lastName == lastName
+

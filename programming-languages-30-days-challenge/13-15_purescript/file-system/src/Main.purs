daa966700791f332312f113bc9f981bbf7ad02ad
Stanislav Sysoev <d4rkr00t@gmail.com>
2017-03-23 09:00:55 +1100

    day(15): PureScript – Tutorial 2

--- programming-languages-30-days-challenge/13-15_purescript/file-system/src/Main.purs
+++ programming-languages-30-days-challenge/13-15_purescript/file-system/src/Main.purs
@@ -1,9 +1,25 @@
 module Main where
 
 import Prelude
+import Data.Path
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
+import Data.Array (concatMap, filter, (:))
+
+allFiles :: Path -> Array Path
+allFiles file = file : concatMap allFiles (ls file)
+
+allFiles' :: Path -> Array Path
+allFiles' file = file : do
+  child <- ls file
+  allFiles' child
+
+onlyFiles :: Path -> Array Path
+onlyFiles file = file : do
+  child <- filter (\f -> not isDirectory f) (ls file)
+  onlyFiles child
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
   log "Hello sailor!"
+

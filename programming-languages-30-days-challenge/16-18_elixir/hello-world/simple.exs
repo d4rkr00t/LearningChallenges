7e3aa717314dd4de0c1b51095d7c02d08e50bd24
Stanislav Sysoev <d4rkr00t@gmail.com>
2017-03-24 08:43:50 +1100

    day(16): Elixir – Hello World

---
+++ programming-languages-30-days-challenge/16-18_elixir/hello-world/simple.exs
@@ -0,0 +1,26 @@
+IO.puts "Hello World from Elixir"
+IO.puts 10 / 2
+IO.puts div 10, 2
+IO.puts rem 10, 2
+IO.puts round(3.58)
+IO.puts trunc(3.58)
+IO.puts :hello #ATOM
+IO.puts "hellö #{:world}"
+IO.puts byte_size("hellö")
+IO.puts String.length("hellö")
+IO.puts String.upcase("hellö")
+
+add = fn a, b -> a + b end
+IO.puts add.(1, 2)
+
+double = fn a -> add.(a, a) end
+IO.puts double.(2)
+
+# IO.puts ([1, 2, 3] ++ [4, 5, 6])
+# IO.puts ([1, true, 2, false, 3, true] -- [true, false])
+
+list = [1, 2, 3]
+IO.puts hd list
+# IO.puts tl list
+
+# IO.puts {:ok, "hello"}

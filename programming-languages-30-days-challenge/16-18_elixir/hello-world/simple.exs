9635c3f236ddcd8229c871cd03d5038aff3e9678
Stanislav Sysoev <d4rkr00t@gmail.com>
2017-03-25 10:50:15 +1100

    day(17): Elixir – Hello World

--- programming-languages-30-days-challenge/16-18_elixir/hello-world/simple.exs
+++ programming-languages-30-days-challenge/16-18_elixir/hello-world/simple.exs
@@ -29,3 +29,17 @@ IO.puts a
 IO.puts b
 IO.puts c
 
+case {1,2,3} do
+    {4,5,6} ->
+        IO.puts "This clause won't match"
+    {1, x, 3} ->
+        IO.puts "This clause will match and bind x to 2 in this clause"
+    _ ->
+        IO.puts "This clause would match any value"
+end
+
+x = 1
+case 10 do
+    ^x -> IO.puts "Won't match"
+    _  -> IO.puts "This clause would match any value"
+end

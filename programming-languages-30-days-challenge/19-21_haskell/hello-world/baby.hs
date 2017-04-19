9616d6c46e94e3c3728469c0e7cfd5fdab96c3aa
Stanislav Sysoev <d4rkr00t@gmail.com>
2017-04-20 09:03:50 +1000

    day(21): Haskell - Hello world

--- programming-languages-30-days-challenge/19-21_haskell/hello-world/baby.hs
+++ programming-languages-30-days-challenge/19-21_haskell/hello-world/baby.hs
@@ -15,4 +15,11 @@ boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x ]
 
 length' xs = sum [ 1 | _ <- xs]
 
+removeNonUppercase :: String -> String
 removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
+
+addThree :: Int -> Int -> Int -> Int
+addThree x y z = x + y + z
+
+factorial :: Integer -> Integer
+factorial n = product [1..n]
